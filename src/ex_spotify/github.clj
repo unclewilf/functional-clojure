78c80e00d5c77255a79762f8233fc84bb42a6e6d
funnellw <Will.Funnell@digitaslbi.com>
2014-03-11 14:22:57 +0000

    Added search functionality over github

--- src/ex_spotify/github.clj
+++ src/ex_spotify/github.clj
@@ -4,6 +4,8 @@
             [cheshire.core :refer [parse-string]]
             [clojure.pprint :refer [pprint]]))
 
+(def github-uri "https://api.github.com")
+
 (defn get-rq
   [url & body]
   (if (empty? body)
@@ -13,11 +15,77 @@
 (defn query-user
   "Run an arbitrary query agains Spotify's API."
   [username]
-  (get-rq (str "https://api.github.com/users/" username)))
+  (get-rq (str github-uri "/users/" username)))
 
-(def myhub (query-user "unclewilf"))
+(defn search-github
+  "Run an arbitrary query agains Spotify's API."
+  [query]
+  (get-rq (str github-uri "/search/repositories?q=" query)))
 
-(keys myhub)
-(vals myhub)
+;query my account
+;(def myhub (query-user "unclewilf"))
+
+(def clojure-search (search-github "clojure"))
+
+(keys clojure-search)
+(vals clojure-search)
+
+(def found-items (:items clojure-search))
+
+(count found-items)
+
+(def order-by-watchers (map :html_url (sort (fn[x y] (> (:watchers_count x)(:watchers_count y))) found-items)))
+
+(map :html_url (sort (fn[x y] (> (:watchers_count x)(:watchers_count y))) found-items))
+
+
+
+(defn urls-and-descriptions
+  [items]
+  (map (fn[item] (hash-map :desc (:description item) :url (:html_url item))) items))
+
+(apply str (interpose ", "(map :desc (urls-and-descriptions found-items))))
+
+
+
+"The top clojure projects"
+(apply str (interpose ", " order-by-watchers))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
